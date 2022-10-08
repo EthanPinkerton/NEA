9ead5925d31356c93e0a9b93b9d6b9cb039b8f02
Ethan <ethanmpinkerton@gmail.com>
2022-10-09 00:10:51 +0100

    updating items

--- src/game/Inventory.java
+++ src/game/Inventory.java
@@ -8,22 +8,43 @@ import java.util.ArrayList;
 public class Inventory {
 
     protected int bombs,teleport,speed,bullets;
-    private Image image;
+    private final Image imageB,imageT,imageS,imageP;
 
     public Inventory(){
         bombs = 1;
-        image = new ImageIcon(this.getClass().getResource("item.jpg")).getImage();
+        teleport = 1;
+        speed = 1;
+        bullets = 1;
+        imageB = new ImageIcon(this.getClass().getResource("bomb.jpg")).getImage();
+        imageT = new ImageIcon(this.getClass().getResource("teleport.jpeg")).getImage();
+        imageS = new ImageIcon(this.getClass().getResource("speed.jpg")).getImage();
+        imageP = new ImageIcon(this.getClass().getResource("bullet.jpg")).getImage();
     }
 
     public void keyPress(KeyListener kl){
         if(kl.isKey1() && bombs > 0){
             bombs -= 1;
         }
+        if(kl.isKey2() && teleport > 0){
+            teleport -= 1;
+        }
+        if(kl.isKey3() && speed > 0){
+            speed -= 1;
+        }
+        if(kl.isKey4() && bullets > 0){
+            bullets -= 1;
+        }
     }
 
     public void draw(Graphics2D g2d, ImageObserver IO){
-        g2d.drawImage(image,10,80,80,80,IO);
+        g2d.drawImage(imageB,10,80,80,80,IO);
         g2d.drawString(Integer.toString(bombs),10,160);
+        g2d.drawImage(imageT,10,160,80,80,IO);
+        g2d.drawString(Integer.toString(teleport),10,240);
+        g2d.drawImage(imageS,10,240,80,80,IO);
+        g2d.drawString(Integer.toString(speed),10,320);
+        g2d.drawImage(imageP,10,320,80,80,IO);
+        g2d.drawString(Integer.toString(bullets),10,400);
     }
 
     public void addItem(char type){
