36268a5a04941416a0fae68b376de4c6ea13899a
mp212353 <ethanmpinkerton@gmail.com>
2022-10-13 10:51:57 +0100

    optimising

--- src/game/GetImage.java
+++ src/game/GetImage.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class GetImage {
     public static Image get(String file){
         try {
-            return new ImageIcon(Objects.requireNonNull(GetImage.class.getResource(file))).getImage();
+            return new ImageIcon(Objects.requireNonNull(GetImage.class.getResource("res/"+file))).getImage();
         }catch (NullPointerException e){
             return new ImageIcon().getImage();
         }
