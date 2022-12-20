6e0a1e5b130339eccab9f52950f1251f7f2eb77c
Ethan <ethanmpinkerton@gmail.com>
2022-12-20 09:45:16 -0800

    adding search bar

--- src/game/Leaderboard.java
+++ src/game/Leaderboard.java
@@ -13,6 +13,9 @@ public class Leaderboard {
     private final JTextArea Score = new JTextArea();
     private final JTextArea Username = new JTextArea();
     private final JTextArea Ongoing = new JTextArea();
+    private final JTextField searchBar = new JTextField();
+    private String displayUsername = "t";
+    private String displayOngoing = "t";
     private int page;
     private final ActionListener previousListener = new ActionListener() {
         @Override
@@ -34,6 +37,37 @@ public class Leaderboard {
         }
     };
 
+    private final ActionListener searchListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String[] text = searchBar.getText().toLowerCase().split(":");
+            switch (text[0]) {
+                case "rank":
+                    if (Integer.parseInt(text[1]) > 0 && Integer.parseInt(text[1]) < allGames[0].length) {
+                        page = (Integer.parseInt(text[1]) - 1) / 25;
+                        display();
+                    }
+                    break;
+                case "id":
+                    displaySearch(allGames[1],text[1]);
+                    break;
+                case "score":
+                    displaySearch(allGames[2],text[1]);
+                    break;
+                case "username":
+                    displayUsername = text[1];
+                    page = 0;
+                    display();
+                    break;
+                case "ongoing":
+                    displayOngoing = text[1];
+                    page = 0;
+                    display();
+                    break;
+            }
+        }
+    };
+
     public Leaderboard(JFrame jFrame, ActionListener backListener) {
         panel = new JPanel();
         panel.setBounds(0, 0, jFrame.getWidth(), jFrame.getHeight());
@@ -45,6 +79,7 @@ public class Leaderboard {
     }
 
     private void format(ActionListener backListener) {
+        JButton search = new JButton("Search");
         JButton previous = new JButton("Previous");
         JButton next = new JButton("Next");
         JButton back = new JButton("Back");
@@ -54,6 +89,8 @@ public class Leaderboard {
         panel.add(Score);
         panel.add(Username);
         panel.add(Ongoing);
+        panel.add(searchBar);
+        panel.add(search);
         panel.add(back);
         panel.add(next);
         panel.add(previous);
@@ -72,12 +109,25 @@ public class Leaderboard {
         Score.setBounds(100, 0, 100, 441);
         Username.setBounds(200, 0, 240, 441);
         Ongoing.setBounds(440, 0, 60, 441);
-        back.setBounds(0, 442, 100, 20);
+        searchBar.setBounds(70, 442, 140, 20);
+        search.setBounds(210, 442, 90, 20);
+        back.setBounds(0, 442, 70, 20);
         previous.setBounds(300, 442, 100, 20);
         next.setBounds(400, 442, 100, 20);
+        search.addActionListener(searchListener);
         back.addActionListener(backListener);
         next.addActionListener(nextListener);
         previous.addActionListener(previousListener);
+    }
+
+    public void displaySearch(String[] array, String item) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(item)) {
+                page = i / 25;
+                display();
+                return;
+            }
+        }
     }
 
     public void removePanel(JFrame jFrame) {
@@ -96,8 +146,20 @@ public class Leaderboard {
     private String getColumn(int index) {
         String rString = allGames[index][0] + "\n\n";
         int len = Math.min(allGames[index].length - 1 - page * 25, 25);
-        for (int i = 1; i < len + 1; i++) {
-            rString += allGames[index][i + page * 25] + "\n";
+        if (displayOngoing.equals("n") && displayUsername.equals("n")) {
+            for (int i = 1; i < len + 1; i++) {
+                rString += allGames[index][i + page * 25] + "\n";
+            }
+        } else {
+            int i = page * 25;
+            int count = 0;
+            while (count < page * 25 + 25 && i < allGames[0].length) {
+                if ((displayUsername.equals("t") || allGames[3][i].toLowerCase().equals(displayUsername)) && (displayOngoing.equals("t") || allGames[4][i].toLowerCase().equals(displayOngoing))) {
+                    rString += allGames[index][i] + "\n";
+                    count++;
+                }
+                i++;
+            }
         }
         return rString;
     }
@@ -107,7 +169,7 @@ public class Leaderboard {
 
         allGames = new String[5][games.length + 1];
 
-        allGames[0][0] = "Index";
+        allGames[0][0] = "Rank";
         allGames[1][0] = "ID";
         allGames[2][0] = "Score";
         allGames[3][0] = "Username";
