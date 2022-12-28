82ad25c0857ba15fc294d5cec209a1440defc84d
Ethan <ethanmpinkerton@gmail.com>
2022-12-27 18:20:47 -0800

    adding logout button

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -11,8 +11,18 @@ public class Menu {
     protected JButton newGame = new JButton("New Game");
     protected JButton loadGame = new JButton("Load game");
     protected JButton leaderboardButton = new JButton("Leaderboard");
+    protected JButton logout = new JButton("Logout");
     protected LoadGame gameLoader;
     protected Leaderboard leaderboard;
+
+    protected ActionListener logoutButton = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            removeComponents();
+            jFrame.repaint();
+            new Login(jFrame);
+        }
+    };
 
     protected ActionListener newGameButton = new ActionListener() {
         @Override
@@ -83,23 +93,28 @@ public class Menu {
         jFrame.add(newGame);
         jFrame.add(loadGame);
         jFrame.add(leaderboardButton);
+        jFrame.add(logout);
         newGame.setBounds(jFrame.getWidth() / 2 - 100, jFrame.getHeight() / 2 - 125, 200, 50);
         loadGame.setBounds(jFrame.getWidth() / 2 - 100, jFrame.getHeight() / 2 - 25, 200, 50);
         leaderboardButton.setBounds(jFrame.getWidth() / 2 - 100, jFrame.getHeight() / 2 + 75, 200, 50);
+        logout.setBounds(jFrame.getWidth() / 2 - 50, jFrame.getHeight() - 57, 100, 20);
         newGame.addActionListener(newGameButton);
         loadGame.addActionListener(loadGameMenuButton);
         leaderboardButton.addActionListener(leaderboardMenuButton);
+        logout.addActionListener(logoutButton);
     }
 
     public void addComponents() {
         jFrame.add(newGame);
         jFrame.add(loadGame);
         jFrame.add(leaderboardButton);
+        jFrame.add(logout);
     }
 
     public void removeComponents() {
         jFrame.remove(newGame);
         jFrame.remove(loadGame);
         jFrame.remove(leaderboardButton);
+        jFrame.remove(logout);
     }
 }
