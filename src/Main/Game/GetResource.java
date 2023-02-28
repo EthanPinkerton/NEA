a5d2967d18e0bc519adcd14baf533b565073f08f
ethan <ethanmpinkerton@gmail.com>
2023-02-28 21:50:35 +0000

    bug fixing

--- src/Main/Game/GetResource.java
+++ src/Main/Game/GetResource.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public final class GetResource {
     public static Image getImage(String file) {
         try {
-            return new ImageIcon(Objects.requireNonNull(GetResource.class.getResource("res/" + file))).getImage();
+            return new ImageIcon(getFile("res/" + file)).getImage();
         } catch (NullPointerException e) {
             return new ImageIcon().getImage();
         }
@@ -15,7 +15,7 @@ public final class GetResource {
 
     public static String getFile(String file) {
         try {
-            return Objects.requireNonNull(GetResource.class.getResource("res/"+file)).getPath().replaceAll("%20", " ");
+            return Objects.requireNonNull(GetResource.class.getResource("res/" + file)).getPath().replaceAll("%20", " ");
         } catch (NullPointerException e) {
             return "";
         }
@@ -28,10 +28,10 @@ public final class GetResource {
     public static JFrame formatJFrame(JFrame jFrame) {
         jFrame.setVisible(false);
         jFrame.setLayout(null);
-        jFrame.setMinimumSize(new Dimension(500,500));
+        jFrame.setMinimumSize(new Dimension(500, 500));
         jFrame.setSize(500, 500);
         //jFrame.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - 250, Toolkit.getDefaultToolkit().getScreenSize().height / 2 - 250);
-        jFrame.setLocation(200,200);
+        jFrame.setLocation(200, 200);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setResizable(false);
         jFrame.setVisible(true);
